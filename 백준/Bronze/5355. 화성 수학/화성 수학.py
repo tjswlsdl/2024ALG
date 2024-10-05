6f5f57b804e6ee9cf07be67cb0c19add46a3cfda
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-10-05 15:58:24 +0900

    [Bronze II] Title: 화성 수학, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,14 @@
+def calc(num, item):
+    if item == '@' : return num * 3
+    elif item == '%' : return num + 5
+    elif item == '#' : return num - 7
+
+n = int(input())
+
+for _ in range(0, n):
+    A = list(input().split(" "))
+    num = float(A.pop(0))
+
+    for i in A:
+        num = calc(num, i)
+    print("%.2f" % num)
