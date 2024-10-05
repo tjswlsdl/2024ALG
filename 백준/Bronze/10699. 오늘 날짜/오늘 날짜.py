c4abdb6a1e7eb84845ecd4168671a853ae549f59
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-10-05 14:39:32 +0900

    [Bronze V] Title: 오늘 날짜, Time: 52 ms, Memory: 38020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -0,0 +1,7 @@
+import datetime
+
+print(str(datetime.datetime.today())[:10]) # today() 함수를 사용해 오늘의 날짜, 시간을 출력
+
+                                                                           # str() 함수를 사용해 출력하려는 내용을 string 형태로 출력
+
+                                                                           # [:10]를 사용해 10글자만 출력하도록 하여 년, 월, 일만 출력하도록  함
