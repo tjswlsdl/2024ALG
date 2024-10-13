f6103f8378de3007ba88b988f923773c350779c4
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-10-13 17:41:20 +0900

    [Silver V] Title: 생일, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.py
@@ -0,0 +1,8 @@
+T = int(input())
+li = []
+for _ in range(T):
+    n, d, m, y = input().split()
+    li.append([n, int(d), int(m), int(y)])
+li.sort(key=lambda x:(x[3],x[2],x[1]))
+print(li[-1][0])
+print(li[0][0])
