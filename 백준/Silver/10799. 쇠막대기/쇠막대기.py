cb1553f5cd1f06568c8cfcc946a1d1a53d2e3025
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-10-19 17:16:49 +0900

    [Silver II] Title: 쇠막대기, Time: 52 ms, Memory: 31552 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,15 +1,20 @@
-ir= input()
+line = input()
 stack=[]
+
 cnt = 0
-for i in range(len(ir)):
-    if ir[i] == "(":
-        stack.append("(")
-    else :
-        if ir[i-1]=="(":
+
+#막대기를 자른 횟수 + 1 = 조각 개수
+#괄호 닫히는 순간이 자르는 순간인디,,
+for i in range(len(line)):
+    if line[i] == '(':
+        stack.append('(')
+        
+    else: 
+        if line[i-1] == '(':
             stack.pop()
-            cnt+=len(stack) # 첫 번째 경우인 현재의 쇠막대기들을 카운팅합니다.
-            
-        else :
+            cnt += len(stack)
+        
+        else: 
             stack.pop()
-            cnt+=1 # 이 부분은 두 번째 경우인 나머지 부분을 세는 것입니다.
+            cnt += 1
 print(cnt)
