80779d2dd39ea8924748d38ce2f090ad5cd540b4
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-11-26 20:35:03 +0900

    [Silver III] Title: 등수 매기기, Time: 444 ms, Memory: 52504 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline())
+vals = []
+for _ in range(n):
+    vals.append(int(sys.stdin.readline()))
+
+vals.sort()
+
+diff = 0
+for i in range(1, n+1):
+    diff += abs(i - vals[i-1])
+
+print(diff)
