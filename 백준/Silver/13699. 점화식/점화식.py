4ff20717d446e5a682a4ac5990cab87c340a9b89
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-11-26 20:10:36 +0900

    [Silver IV] Title: 점화식, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] *(n+1)
+dp[0] = 1
+
+for i in range(1,n+1):
+    for j in range(i):
+        dp[i] += dp[j]*dp[i-1-j]
+print(dp[n])
