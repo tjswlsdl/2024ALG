dd5f857add89e75333267487e8e4f58308ab5c4c
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-11-29 20:28:00 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 1060 ms, Memory: 148436 KB -BaekjoonHub

--- 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
@@ -1,12 +1,15 @@
 N = int(input())
-c = list(map(int, input().split()))
+num = list(map(int, input().split()))
 k = int(input())
- 
+
+
 index = N // k
 arr = []
- 
-for i in range(0,N,index):
-    arr = c[i:i+index]
+
+
+for i in range(0,N,index) :
+    arr = num[i:i+index]
     arr.sort()
+    
     for j in arr:
-        print(j, end=' ')
+        print(j, end =' ')
