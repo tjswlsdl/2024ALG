4032afc6399c12c6244f0aa92083f16313acf768
tjswlsdl <166943752+tjswlsdl@users.noreply.github.com>
2024-12-07 00:00:16 +0900

    [Silver V] Title: 최대 상승, Time: 172 ms, Memory: 56452 KB -BaekjoonHub

--- 백준/Silver/25644. 최대 상승/최대 상승.py
+++ 백준/Silver/25644. 최대 상승/최대 상승.py
@@ -1,14 +1,11 @@
+# 25214와 같은 문제.
 import sys
 input = sys.stdin.readline
-
 N = int(input())
-high = 0
-result = 0
-
-ANA = list(map(int, input().split()))
-    
-for i in range(N-1,-1,-1):
-    high = max(high, ANA[i])
-    result = max(result, high -ANA[i] )
-    
-print(result)
+A = list(map(int,input().split()))
+dp = [0] * N # dp 초기화
+small = A[0] # 
+for i in range(1, N):
+    small = min(small, A[i]) # 추가 될 때 마다 최솟값 찾기
+    dp[i] = max(dp[i-1], A[i] - small) # 이전 최댓값 vs 이번에 구한거
+print(max(dp)) 
